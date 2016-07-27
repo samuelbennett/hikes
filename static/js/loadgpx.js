7f74291192e5956c0ea1137d60a42f8beeae1fe0
Samuel Bennett <samuelbennett93@gmail.com>
2016-07-27 21:37:06 +0100

    add attribution

--- static/js/loadgpx.js
+++ static/js/loadgpx.js
@@ -1,3 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////
+// loadgpx.4.js
+//
+// Javascript object to load GPX-format GPS data into Google Maps.
+//
+// Copyright (C) 2006 Kaz Okuda (http://notions.okuda.ca)
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// If you use this script or have any questions please leave a comment
+// at http://notions.okuda.ca/geotagging/projects-im-working-on/gpx-viewer/
+// A link to the GPL license can also be found there.
+//
+///////////////////////////////////////////////////////////////////////////////
+//
+// History:
+//    revision 1 - Initial implementation
+//    revision 2 - Removed LoadGPXFileIntoGoogleMap and made it the callers
+//                 responsibility.  Added more options (colour, width, delta).
+//    revision 3 - Waypoint parsing now compatible with Firefox.
+//    revision 4 - Upgraded to Google Maps API version 2.  Tried changing the way
+//               that the map calculated the way the center and zoom level, but
+//               GMAP API 2 requires that you center and zoom the map first.
+//               I have left the bounding box calculations commented out in case
+//               they might come in handy in the future.
+//
+//    5/28/2010 - Upgraded to Google Maps API v3 and refactored the file a bit.
+//                          (Chris Peplin)
+//
+// Author: Kaz Okuda
+// URI: http://notions.okuda.ca/geotagging/projects-im-working-on/gpx-viewer/
+//
+// Updated for Google Maps API v3 by Chris Peplin
+// Fork moved to GitHub: https://github.com/peplin/gpxviewer
+//
+///////////////////////////////////////////////////////////////////////////////
+
 function GPXParser(xmlDoc, map) {
     this.xmlDoc = xmlDoc;
     this.map = map;
